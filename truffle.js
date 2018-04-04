afdd9a00cb3f8f8a957647fd52cd001bb006ff20
gasolin <gasolin@gmail.com>
2018-04-04 21:25:00 +0800

    add placeholder truffle.js for the truffle unbox command

---
+++ truffle.js
@@ -0,0 +1,5 @@
+module.exports = {
+  // placeholder, no function here
+  // this file is essential for the truffle unbox command
+  // should fix in https://github.com/trufflesuite/truffle-box/issues/11
+};
