6d4fa1e2aed0baea1e1738c701c23b1ec27a7825
gasolin <gasolin@gmail.com>
2018-04-04 21:54:44 +0800

    fix header wording and update the test

--- web/src/components/Header.js
+++ web/src/components/Header.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 
 export function Header (props) {
   let noConnectionMsg = props.noConnection === true ?
-    'Not connected to the blockchain' : 'Connected to the blockchain';
+    'Not connect to the blockchain' : 'Connected to the blockchain';
   return(
     <header className="App-header">
       <h1 className="App-title">{noConnectionMsg}</h1>
