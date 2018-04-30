d56479440f3043459cff22519c3efadbd8486d21
gasolin <gasolin@gmail.com>
2018-05-01 02:07:59 +0800

    lint fix

--- web/src/App.js
+++ web/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 // locale
 import { addLocaleData, IntlProvider } from 'react-intl';
