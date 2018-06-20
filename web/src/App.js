212eaa07c71481e5067ab508a564789eae042bf9
gasolin <gasolin@gmail.com>
2018-06-20 19:52:32 +0800

    move getLocale into locales

--- web/src/App.js
+++ web/src/App.js
@@ -1,46 +1,29 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-
-// locale
-import { addLocaleData, IntlProvider } from 'react-intl';
-import enUS from './locales/en-US';
-import zhHANT from './locales/zh-Hant';
+import { IntlProvider } from 'react-intl';
+import { getLocale } from 'locales'
 
 import './App.css';
 import NoMatch from './components/NoMatch';
 import Home from './containers/Home';
 // import Counter from './components/About';
 
-addLocaleData(zhHANT.data);
-addLocaleData(enUS.data);
+function App() {
+  const locale = getLocale();
 
-export class App extends Component {
-  getLocale =() => {
-    const lang = localStorage.getItem('lang');
-    switch (lang) {
-      case 'zhHANT':
-        return zhHANT;
-      default:
-        return enUS;
-    }
-  }
-
-  render() {
-    const locale = this.getLocale();
-    return (
-      <IntlProvider locale={locale.locale} messages={locale.messages}>
-        <Router basename="/">
-          <div className="App">
-            <Switch>
-              <Route exact path="/" component={Home} />
-              {/* <Route path="/about" component={About} /> */}
-              <Route component={NoMatch} />
-            </Switch>
-          </div>
-        </Router>
-      </IntlProvider>
-    );
-  }
+  return (
+    <IntlProvider locale={locale.locale} messages={locale.messages}>
+      <Router basename="/">
+        <div className="App">
+          <Switch>
+            <Route exact path="/" component={Home} />
+            {/* <Route path="/about" component={About} /> */}
+            <Route component={NoMatch} />
+          </Switch>
+        </div>
+      </Router>
+    </IntlProvider>
+  );
 }
 
 export default App;
